a2ba88b9bbd68dc076d7c5a018d5178d97bfefc4
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2025-08-05 00:35:40 +0530

    updated

---
+++ hardhat.config.ts
@@ -0,0 +1,18 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.20",
+  networks: {
+    crossfiTestnet: {
+      url: "https://rpc.testnet.ms",
+      chainId: 4157,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
